36cf2baa2b9f0be14b70345c8a3639e4bfd8e723
Sizno <Sizno@Sizno-PC>
2015-04-13 11:31:45 +0900

    과제 제출

---
+++ JavaPractice/src/org/dimigo/oop/Car.java
@@ -0,0 +1,68 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+ *<pre>
+ *
+ * JavaPractice
+ * org.dimigo.oop
+ *  |_ Car
+ *
+ * 1.  개요 :
+ * 2. 작성일: 2015. 4. 13.
+ *
+ *
+ * </pre>
+ * @author   : 박수환
+ * @version  : 1.0
+ */
+public class Car {
+	
+	private String company;
+	private String model;
+	private String color;
+	private int maxSpeed;
+	private int price;
+	
+	public String getCompany() {
+		return company;
+	}
+	public String getModel() {
+		return model;
+	}
+	public String getColor() {
+		return color;
+	}
+	public int getMaxSpeed() {
+		return maxSpeed;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setCompany(String setcompany) {
+		company = setcompany;
+	}
+	public void setModel(String setmodel) {
+		model = setmodel;
+	}
+	public void setColor(String setcolor) {
+		color = setcolor;
+	}
+	public void setMaxSpeed(int setmaxspeed) {
+		maxSpeed = setmaxspeed;
+	}
+	public void setPrice( int setprice) {
+		price = setprice;
+	}
+	/**
+	 * @param setmaxspeed
+	 */
+	
+	
+	
+	
+	
+
+}
---
+++ JavaPractice/src/org/dimigo/oop/CarTest.java
@@ -0,0 +1,84 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+ *<pre>
+ *
+ * JavaPractice
+ * org.dimigo.oop
+ *  |_ CarTest
+ *
+ * 1.  개요 :
+ * 2. 작성일: 2015. 4. 13.
+ *
+ *
+ * </pre>
+ * @author   : 박수환
+ * @version  : 1.0
+ */
+public class CarTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		Car hd = new Car();
+		Car kia = new Car();
+		Car ss = new Car();
+		
+		
+		hd.setCompany("현대자동차");
+		hd.setModel("제네시스");
+		hd.setColor("검정색");
+		hd.setMaxSpeed(225);
+		hd.setPrice(50000000);
+		
+		kia.setCompany("기아자동차");
+		kia.setModel("K7");
+		kia.setColor("흰색");
+		kia.setMaxSpeed(246);
+		kia.setPrice(40000000);
+		
+		ss.setCompany("삼성자동차");
+		ss.setModel("SM7");
+		ss.setColor("회색");
+		ss.setMaxSpeed(200);
+		ss.setPrice(38000000);
+		
+		
+		System.out.println("<< 자동차 목록 >>");
+		System.out.println("제조사명 : " + hd.getCompany() );
+		System.out.println("모델명 :" + hd.getModel() );
+		System.out.println("색상 : " + hd.getColor() );
+		System.out.println("최대속도 : " + hd.getMaxSpeed());
+		System.out.println("가격 : " + String.format("%,d",hd.getPrice()) );
+		System.out.println("");
+		
+		System.out.println("<< 자동차 목록 >>");
+		System.out.println("제조사명 : " + kia.getCompany());
+		System.out.println("모델명 :" + kia.getModel() );
+		System.out.println("색상 : " + kia.getColor() );
+		System.out.println("최대속도 : " +kia.getMaxSpeed());
+		System.out.println("가격 : " +String.format("%,d", kia.getPrice()) );
+		System.out.println("");
+		
+		System.out.println("<< 자동차 목록 >>");
+		System.out.println("제조사명 : " + ss.getCompany());
+		System.out.println("모델명 :" + ss.getModel() );
+		System.out.println("색상 : " + ss. getColor());
+		System.out.println("최대속도 : " + ss.getMaxSpeed());
+		System.out.println("가격 : " + String.format("%,d", ss.getPrice()));
+		System.out.println("");
+		
+		
+
+		
+		
+		
+
+	}
+
+}
