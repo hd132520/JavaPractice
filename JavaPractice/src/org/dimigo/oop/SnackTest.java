a6d6e4a6bdc494832e9f4d02b0ec117e9e1e9723
Sizno <Sizno@Sizno-PC>
2015-05-18 11:55:28 +0900

    과제제출

---
+++ JavaPractice/src/org/dimigo/oop/SnackTest.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+ *<pre>
+ *
+ * JavaPractice
+ * org.dimigo.oop
+ *  |_ SnackTest
+ *
+ * 1.  개요 :
+ * 2. 작성일: 2015. 5. 18.
+ *
+ *
+ * </pre>
+ * @author   : 박수환
+ * @version  : 1.0
+ */
+public class SnackTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		Snack[] snackArr = new Snack[]{
+				new Snack("새우깡","농심",1100,2),
+				new Snack("콘칲","크라운",1200,1),
+				new Snack("허니버터칩","해태",1500,4)
+		};
+		
+		
+		
+		
+		
+		for(Snack val: snackArr) {
+			val.printSnack();
+			System.out.println();
+		}System.out.println();
+		
+	
+	
+		
+
+	}
+
+	/**
+	 * 
+	 */
+
+}
