cdb4363177d37bcffe5282dea6df39367b15431e
Sizno <Sizno@Sizno-PC>
2015-08-12 15:51:54 +0900

    과제 제출

---
+++ JavaPractice/src/org/dimigo/inheritance/FigureTest.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+/**
+ *<pre>
+ *
+ * JavaPractice
+ * org.dimigo.inheritance
+ *  |_ FigureTest
+ *
+ * 1.  개요 :
+ * 2. 작성일: 2015. 8. 12.
+ *
+ *
+ * </pre>
+ * @author   : 박수환
+ * @version  : 1.0
+ */
+public class FigureTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		Circle c = new Circle(5);
+		System.out.println("원의 넓이 : " +String.format("%.1f",c.calcArea()));
+		
+		Triangle t = new Triangle(10,10,5,8);
+		System.out.println("삼각형 넓이 : " +t.calcArea());
+		
+		Rectangle r  = new Rectangle(20,20,5,8);
+		System.out.println("사각형 넓이 : " +r.calcArea());
+
+	}
+
+}
