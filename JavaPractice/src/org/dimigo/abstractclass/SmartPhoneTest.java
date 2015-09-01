95351081dd3dd4e28adbb09ec71d38ab2e8f1570
Sizno <Sizno@Sizno-PC>
2015-09-01 10:18:07 +0900

    과제제출

---
+++ JavaPractice/src/org/dimigo/abstractclass/SmartPhoneTest.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package org.dimigo.abstractclass;
+
+/**
+ *<pre>
+ *
+ * JavaPractice
+ * org.dimigo.inheritance
+ *  |_ SmartPhoneTest
+ *
+ * 1.  개요 :
+ * 2. 작성일: 2015. 8. 25.
+ *
+ *
+ * </pre>
+ * @author   : 박수환
+ * @version  : 1.0
+ */
+public class SmartPhoneTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SmartPhone[] phones = {
+				new IPhone("IPhone 6","Apple",700000),
+				new Galaxy("갤럭시 S6","삼성",650000)};
+				
+		for(SmartPhone msi : phones) {
+			System.out.println(msi);
+			msi.turnOn();
+			msi.pay();
+			msi.useSpecialFunction(msi);
+			msi.turnOff();
+			System.out.println();
+			
+		}
+				
+		}
+
+	}
